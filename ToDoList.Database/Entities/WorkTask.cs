a1dde58ff89f629ff72b37cdf8f7047b26613aec
Karol Napierala <karoln109@gmail.com>
2022-01-17 14:24:41 +0100

    To Do List

---
+++ ToDoList.Database/Entities/WorkTask.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Database
+{
+    public class WorkTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ ToDoList.Database/ToDoListDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Database
+{
+    public class ToDoListDbContext : DbContext
+    {
+        public DbSet<WorkTask> WorkTasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite($"Filename={Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments), "ToDoListApp.sqlite")}");
+        }
+    }
+}
